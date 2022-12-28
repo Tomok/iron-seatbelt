093c9f446647206d14676467339ac85542062e68
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2022-12-28 16:56:30 +0000

    reworked expression parsing to use intermediate parse into tokens

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod parser_combinator {
+
     use nom::{
         branch::alt,
         bytes::complete::{tag, take_while1},
@@ -56,7 +57,6 @@ pub mod parser_combinator {
                 map(Import::parse_span, Self::Import),
                 map(Function::parse_span, Self::Function),
             ))(s)?;
-            dbg!((&s, &res));
             Ok((s, res))
         }
 
@@ -244,6 +244,7 @@ pub mod parser_combinator {
                     })) => {
                         //is parameter?
                         if !next_param_allowed {
+                            dbg!(input);
                             dbg!("No more paramenters allowed");
                             //todo: how to state that a `,` or a `)` were expected?
                             //return Err(nom::Err::Error(Error::new(input, ErrorKind::Tag)));
@@ -657,46 +658,316 @@ pub mod parser_combinator {
         >(
             s: Span<'s>,
         ) -> IResult<Span, Self, E> {
-            alt((
-                map(IntLiteral::parse_span, Self::IntLiteral),
-                map(CharLiteral::parse_span, Self::CharLiteral),
-                map(FunctionCall::parse_span, Self::FunctionCall),
-                map(Ident::parse_span, Self::Ident),
-            ))(s)
+            let (remaining, tokens) = many1(ExpressionToken::parse_span)(s)?;
+            let expr = Expression::from_tokens(tokens.as_slice())?;
+            Ok((remaining, expr))
+        }
+
+        fn from_tokens<
+            'v,
+            E: ParseError<LocatedSpan<&'s str>> + ContextError<LocatedSpan<&'s str>>,
+        >(
+            values: &'v [ExpressionToken<'s>],
+        ) -> Result<Self, nom::Err<E>> {
+            if let [value] = values {
+                match value {
+                    ExpressionToken::IntLiteral(v) => Ok(Self::IntLiteral(v.clone())),
+                    ExpressionToken::CharLiteral(v) => Ok(Self::CharLiteral(v.clone())),
+                    ExpressionToken::Braces(_) => todo!(),
+                    ExpressionToken::Operator(_) => todo!(),
+                    ExpressionToken::Ident(v) => Ok(Self::Ident(v.clone())),
+                }
+            } else {
+                //more than one token ... need to build tree
+                // check if this is a function - for function calls the last call is the outermost
+                if let Some((maybe_params, function_expr_tokens)) = values.split_last() {
+                    let maybe_params: &'v _ = maybe_params;
+                    let function_expr_tokens: &'v _ = function_expr_tokens;
+                    if let Some(maybe_params) = maybe_params.as_braces() {
+                        let maybe_params: &'v _ = maybe_params;
+                        let call =
+                            FunctionCall::from_expr_and_braces(function_expr_tokens, maybe_params)?;
+                        return Ok(Expression::FunctionCall(call));
+                    }
+                    todo!()
+                }
+                todo!()
+            }
         }
     }
 
-    #[derive(PartialEq, Eq, Debug)]
-    pub struct FunctionCall<'s> {
-        function_expr: Box<Expression<'s>>, //does this make sense? is there a more specific type,
-        //that could be used?
-        open_brace: Span<'s>,
-        parameters: Vec<FunctionCallParameter<'s>>,
-        closing_brace: Span<'s>,
+    ///due to possible recursion, e.g. `a()()` it is easier to parse expressions into tokens first
+    ///and then generate the AST based on that
+    #[derive(PartialEq, Eq, Debug, Clone)]
+    enum ExpressionToken<'s> {
+        IntLiteral(IntLiteral<'s>),
+        CharLiteral(CharLiteral<'s>),
+        Braces(BracesTokens<'s>),
+        Operator(OperatorToken<'s>),
+        /// an Ident, could be a variable_name or a function name
+        Ident(Ident<'s>),
     }
 
-    impl<'s> FunctionCall<'s> {
+    impl<'s> ExpressionToken<'s> {
         pub fn parse_span<
             E: ParseError<LocatedSpan<&'s str>> + ContextError<LocatedSpan<&'s str>>,
         >(
             s: Span<'s>,
         ) -> IResult<Span, Self, E> {
-            let (s, expr) = Expression::parse_span(s)?;
-            let function_expr = Box::new(expr);
+            alt((
+                map(IntLiteral::parse_span, Self::IntLiteral),
+                map(CharLiteral::parse_span, Self::CharLiteral),
+                map(BracesTokens::parse_span, Self::Braces),
+                map(OperatorToken::parse_span, Self::Operator),
+                map(Ident::parse_span, Self::Ident),
+            ))(s)
+        }
+
+        fn as_operator(&self) -> Option<&OperatorToken<'s>> {
+            if let Self::Operator(v) = self {
+                Some(v)
+            } else {
+                None
+            }
+        }
+
+        #[must_use]
+        fn is_comma_operator(&self) -> bool {
+            matches!(self, Self::Operator(OperatorToken::Comma(_)))
+        }
+
+        #[must_use]
+        fn as_comma_operator<'a>(&'a self) -> Option<&'a Span<'s>> {
+            self.as_operator()
+                .filter(|t| t.is_comma())
+                .map(OperatorToken::span)
+        }
+
+        fn as_braces(&self) -> Option<&BracesTokens<'s>> {
+            if let Self::Braces(v) = self {
+                Some(v)
+            } else {
+                None
+            }
+        }
+    }
+
+    #[derive(PartialEq, Eq, Debug, Clone)]
+    struct BracesTokens<'s> {
+        open_brace: Span<'s>,
+        contents: Vec<ExpressionToken<'s>>,
+        closing_brace: Span<'s>,
+    }
+
+    impl<'s> BracesTokens<'s> {
+        pub fn parse_span<
+            E: ParseError<LocatedSpan<&'s str>> + ContextError<LocatedSpan<&'s str>>,
+        >(
+            s: Span<'s>,
+        ) -> IResult<Span, Self, E> {
             let (s, open_brace) = tag("(")(s)?;
             let (s, _) = multispace0(s)?;
-            let (s, parameters) = many0(FunctionCallParameter::parse_span)(s)?;
-            let (s, closing_brace) = tag(")")(s)?;
+            let (s, contents) = many0(ExpressionToken::parse_span)(s)?;
             let (s, _) = multispace0(s)?;
+            let (s, closing_brace) = tag(")")(s)?;
             Ok((
                 s,
                 Self {
-                    function_expr,
                     open_brace,
-                    parameters,
+                    contents,
                     closing_brace,
                 },
             ))
+        }
+    }
+    #[derive(PartialEq, Eq, Debug, Clone)]
+    enum OperatorToken<'s> {
+        Add(Span<'s>),
+        Sub(Span<'s>),
+        Mul(Span<'s>),
+        Div(Span<'s>),
+        Mod(Span<'s>),
+
+        And(Span<'s>),
+        Or(Span<'s>),
+
+        Eq(Span<'s>),
+        /// `!=`
+        Neq(Span<'s>),
+        Le(Span<'s>),
+        Ge(Span<'s>),
+        Lt(Span<'s>),
+        Gt(Span<'s>),
+
+        Comma(Span<'s>),
+    }
+
+    impl<'s> OperatorToken<'s> {
+        pub fn parse_span<
+            E: ParseError<LocatedSpan<&'s str>> + ContextError<LocatedSpan<&'s str>>,
+        >(
+            s: Span<'s>,
+        ) -> IResult<Span, Self, E> {
+            alt((
+                map(tag("+"), Self::Add),
+                map(tag("-"), Self::Sub),
+                map(tag("*"), Self::Mul),
+                map(tag("/"), Self::Div),
+                map(tag("%"), Self::Mod),
+                map(tag("And"), Self::And),
+                map(tag("Or"), Self::Or),
+                map(tag("=="), Self::Eq),
+                map(tag("!="), Self::Neq),
+                map(tag("<="), Self::Le),
+                map(tag(">="), Self::Ge),
+                map(tag("<"), Self::Lt),
+                map(tag(">"), Self::Gt),
+                map(tag(","), Self::Comma),
+            ))(s)
+        }
+
+        #[must_use]
+        fn span<'a>(&'a self) -> &'a Span<'s> {
+            match self {
+                OperatorToken::Add(s)
+                | OperatorToken::Sub(s)
+                | OperatorToken::Mul(s)
+                | OperatorToken::Div(s)
+                | OperatorToken::Mod(s)
+                | OperatorToken::And(s)
+                | OperatorToken::Or(s)
+                | OperatorToken::Eq(s)
+                | OperatorToken::Neq(s)
+                | OperatorToken::Le(s)
+                | OperatorToken::Ge(s)
+                | OperatorToken::Lt(s)
+                | OperatorToken::Gt(s)
+                | OperatorToken::Comma(s) => s,
+            }
+        }
+
+        /// Returns `true` if the operator token is [`Add`].
+        ///
+        /// [`Add`]: OperatorToken::Add
+        #[must_use]
+        fn is_add(&self) -> bool {
+            matches!(self, Self::Add(..))
+        }
+
+        /// Returns `true` if the operator token is [`Sub`].
+        ///
+        /// [`Sub`]: OperatorToken::Sub
+        #[must_use]
+        fn is_sub(&self) -> bool {
+            matches!(self, Self::Sub(..))
+        }
+
+        /// Returns `true` if the operator token is [`Mul`].
+        ///
+        /// [`Mul`]: OperatorToken::Mul
+        #[must_use]
+        fn is_mul(&self) -> bool {
+            matches!(self, Self::Mul(..))
+        }
+
+        /// Returns `true` if the operator token is [`Div`].
+        ///
+        /// [`Div`]: OperatorToken::Div
+        #[must_use]
+        fn is_div(&self) -> bool {
+            matches!(self, Self::Div(..))
+        }
+
+        /// Returns `true` if the operator token is [`Mod`].
+        ///
+        /// [`Mod`]: OperatorToken::Mod
+        #[must_use]
+        fn is_mod(&self) -> bool {
+            matches!(self, Self::Mod(..))
+        }
+
+        /// Returns `true` if the operator token is [`Or`].
+        ///
+        /// [`Or`]: OperatorToken::Or
+        #[must_use]
+        fn is_or(&self) -> bool {
+            matches!(self, Self::Or(..))
+        }
+
+        /// Returns `true` if the operator token is [`Eq`].
+        ///
+        /// [`Eq`]: OperatorToken::Eq
+        #[must_use]
+        fn is_eq(&self) -> bool {
+            matches!(self, Self::Eq(..))
+        }
+
+        /// Returns `true` if the operator token is [`Neq`].
+        ///
+        /// [`Neq`]: OperatorToken::Neq
+        #[must_use]
+        fn is_neq(&self) -> bool {
+            matches!(self, Self::Neq(..))
+        }
+
+        /// Returns `true` if the operator token is [`Comma`].
+        ///
+        /// [`Comma`]: OperatorToken::Comma
+        #[must_use]
+        fn is_comma(&self) -> bool {
+            matches!(self, Self::Comma(..))
+        }
+    }
+
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct FunctionCall<'s> {
+        callee: Box<Expression<'s>>, //does this make sense? is there a more specific type,
+        //that could be used?
+        parameters: FunctionCallParameters<'s>,
+    }
+
+    impl<'s> FunctionCall<'s> {
+        fn from_expr_and_braces<'v, E>(
+            function_expr_tokens: &'v [ExpressionToken<'s>],
+            maybe_params: &'v BracesTokens<'s>,
+        ) -> Result<FunctionCall<'s>, nom::Err<E>>
+        where
+            E: ParseError<LocatedSpan<&'s str>> + ContextError<LocatedSpan<&'s str>>,
+        {
+            let parameters = FunctionCallParameters::from_tokens(maybe_params)?;
+            let callee = Box::new(Expression::from_tokens(function_expr_tokens)?);
+            Ok(Self { callee, parameters })
+        }
+    }
+
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct FunctionCallParameters<'s> {
+        open_brace: Span<'s>,
+        parameters: Vec<FunctionCallParameter<'s>>,
+        closing_brace: Span<'s>,
+    }
+
+    impl<'s> FunctionCallParameters<'s> {
+        fn from_tokens<
+            'v,
+            E: ParseError<LocatedSpan<&'s str>> + ContextError<LocatedSpan<&'s str>>,
+        >(
+            value: &'v BracesTokens<'s>,
+        ) -> Result<Self, nom::Err<E>> {
+            let open_brace = value.open_brace;
+            let closing_brace = value.closing_brace;
+            let mut parameters = Vec::new();
+            for param in value
+                .contents
+                .split_inclusive(ExpressionToken::is_comma_operator)
+            {
+                parameters.push(FunctionCallParameter::from_tokens(param)?);
+            }
+            Ok(Self {
+                open_brace,
+                parameters,
+                closing_brace,
+            })
         }
     }
 
@@ -704,6 +975,40 @@ pub mod parser_combinator {
     pub struct FunctionCallParameter<'s> {
         expression: Box<Expression<'s>>,
         comma: Option<Span<'s>>,
+    }
+
+    impl<'s> FunctionCallParameter<'s> {
+        fn from_tokens<
+            'v,
+            E: ParseError<LocatedSpan<&'s str>> + ContextError<LocatedSpan<&'s str>>,
+        >(
+            value: &'v [ExpressionToken<'s>],
+        ) -> Result<Self, nom::Err<E>> {
+            assert!(!value.is_empty()); //should only be called, if there is a parameter
+            if let Some((last, expression_tokens)) = value.split_last() {
+                let expression_tokens: &'v _ = expression_tokens;
+                if let Some(comma) = last.as_comma_operator() {
+                    let expression = Box::new(Expression::from_tokens(expression_tokens)?);
+                    Ok(Self {
+                        expression,
+                        comma: Some(Clone::clone(comma)),
+                    })
+                } else {
+                    let expression = Box::new(Expression::from_tokens(value)?);
+                    Ok(Self {
+                        expression,
+                        comma: None,
+                    })
+                }
+            } else {
+                // only one element
+                let expression = Box::new(Expression::from_tokens(value)?);
+                Ok(Self {
+                    expression,
+                    comma: None,
+                })
+            }
+        }
     }
 
     impl<'s> FunctionCallParameter<'s> {
@@ -938,6 +1243,9 @@ pub mod parser_combinator {
         ) -> IResult<Span, Self, E> {
             let (s, ident) = take_while1(AsChar::is_alphanum)(s)?;
             //todo: check that ident does not start with a number
+            //
+            //check that ident is not a reserved word
+            assert!("if" != *ident);
             Ok((s, Self(ident)))
         }
     }
