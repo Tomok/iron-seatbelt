602bc5b07e027dbb966ac05a9837fc91d72fb905
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2023-01-21 15:17:45 +0000

    added is_ and as_ to Expression

--- src/parser_combinator/expression.rs
+++ src/parser_combinator/expression.rs
@@ -37,6 +37,144 @@ pub enum Expression<'a> {
     IdentPath(IdentPath<'a>),
 }
 
+impl<'a> Expression<'a> {
+    /// Returns `true` if the expression is [`UnaryOperation`].
+    ///
+    /// [`UnaryOperation`]: Expression::UnaryOperation
+    #[must_use]
+    pub fn is_unary_operation(&self) -> bool {
+        matches!(self, Self::UnaryOperation(..))
+    }
+
+    pub fn as_unary_operation(&self) -> Option<&UnaryOperation<'a>> {
+        if let Self::UnaryOperation(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Returns `true` if the expression is [`FunctionCall`].
+    ///
+    /// [`FunctionCall`]: Expression::FunctionCall
+    #[must_use]
+    pub fn is_function_call(&self) -> bool {
+        matches!(self, Self::FunctionCall(..))
+    }
+
+    pub fn as_function_call(&self) -> Option<&FunctionCall<'a>> {
+        if let Self::FunctionCall(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Returns `true` if the expression is [`BinaryOperation`].
+    ///
+    /// [`BinaryOperation`]: Expression::BinaryOperation
+    #[must_use]
+    pub fn is_binary_operation(&self) -> bool {
+        matches!(self, Self::BinaryOperation(..))
+    }
+
+    pub fn as_binary_operation(&self) -> Option<&BinaryOperation<'a>> {
+        if let Self::BinaryOperation(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Returns `true` if the expression is [`BracketOperation`].
+    ///
+    /// [`BracketOperation`]: Expression::BracketOperation
+    #[must_use]
+    pub fn is_bracket_operation(&self) -> bool {
+        matches!(self, Self::BracketOperation(..))
+    }
+
+    /// Returns `true` if the expression is [`Continue`].
+    ///
+    /// [`Continue`]: Expression::Continue
+    #[must_use]
+    pub fn is_continue(&self) -> bool {
+        matches!(self, Self::Continue(..))
+    }
+
+    pub fn as_continue(&self) -> Option<&Continue<'a>> {
+        if let Self::Continue(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Returns `true` if the expression is [`Break`].
+    ///
+    /// [`Break`]: Expression::Break
+    #[must_use]
+    pub fn is_break(&self) -> bool {
+        matches!(self, Self::Break(..))
+    }
+
+    pub fn as_break(&self) -> Option<&Break<'a>> {
+        if let Self::Break(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Returns `true` if the expression is [`IntLiteral`].
+    ///
+    /// [`IntLiteral`]: Expression::IntLiteral
+    #[must_use]
+    pub fn is_int_literal(&self) -> bool {
+        matches!(self, Self::IntLiteral(..))
+    }
+
+    pub fn as_int_literal(&self) -> Option<&IntLiteral<'a>> {
+        if let Self::IntLiteral(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Returns `true` if the expression is [`CharLiteral`].
+    ///
+    /// [`CharLiteral`]: Expression::CharLiteral
+    #[must_use]
+    pub fn is_char_literal(&self) -> bool {
+        matches!(self, Self::CharLiteral(..))
+    }
+
+    pub fn as_char_literal(&self) -> Option<&CharLiteral<'a>> {
+        if let Self::CharLiteral(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Returns `true` if the expression is [`IdentPath`].
+    ///
+    /// [`IdentPath`]: Expression::IdentPath
+    #[must_use]
+    pub fn is_ident_path(&self) -> bool {
+        matches!(self, Self::IdentPath(..))
+    }
+
+    pub fn as_ident_path(&self) -> Option<&IdentPath<'a>> {
+        if let Self::IdentPath(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+}
+
 impl<'a> FromSpan<'a> for Expression<'a> {
     fn parse_span<E: ParseError<LocatedSpan<&'a str>> + ContextError<LocatedSpan<&'a str>>>(
         s: Span<'a>,
